e0ef7d36f65a621ebd029c1ed4a4b5e8f2664e8f
nckappdev <thatdeveloperguy1301@gmail.com>
2020-08-16 20:58:33 +0100

    drawer included

--- app/src/main/java/com/example/ecommerce_java/MainActivity.java
+++ app/src/main/java/com/example/ecommerce_java/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ecommerce_java;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -12,12 +15,34 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding activityMainBinding;
   private  DrawerLayout drawerLayout;
 
+  private ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
         activityMainBinding  = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
         drawerLayout = activityMainBinding.drawerLayout;
+       actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+       drawerLayout.addDrawerListener(actionBarDrawerToggle);
+       actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return  true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
